dd475d278204fdb879ca52d34410e0b42eb2370b
DrShahinstein <1taylanozturk@protonmail.com>
2025-07-17 00:20:34 +0300

    Update params of mouse_callback()

--- include/helper.hpp
+++ include/helper.hpp
@@ -6,7 +6,7 @@
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void camera_callback(GLFWwindow *window, int key, int scancode, int action, int mods, glm::vec3 &camera_pos, glm::vec3 &camera_front, glm::vec3 &camera_up);
-void mouse_callback(GLFWwindow *window, double xpos, double ypos, float &yaw, float &pitch, glm::vec3 &camera_front);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos, float &yaw, float &pitch, glm::vec3 &camera_front, bool &reset_mouse_position);
 const char *load_shader(const char *shader_path);
 
 #endif
