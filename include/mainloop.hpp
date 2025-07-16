42027354f646bfcdb1ff94c2f81941586e983ebc
DrShahinstein <1taylanozturk@protonmail.com>
2025-07-17 00:24:06 +0300

    Create AppState structure

--- include/mainloop.hpp
+++ include/mainloop.hpp
@@ -2,6 +2,17 @@
 #define MAINLOOP_HPP
 
 #include <GLFW/glfw3.h>
+#include "simulation.hpp"
+
+struct AppState{
+  Simulation simulation;
+  bool is_mouse_captured;
+  bool is_paused;
+  bool gui_visible;
+  float simulation_speed = 1.0f;
+  double mouse_x = 0.0;
+  double mouse_y = 0.0;
+};
 
 void mainloop(GLFWwindow *window);
 
