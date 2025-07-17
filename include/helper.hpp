fa8f23ec3b7f275fff0f7f56140af62f74c7e024
DrShahinstein <1taylanozturk@protonmail.com>
2025-07-17 20:21:37 +0300

    Return std::string while loading shaders

--- include/helper.hpp
+++ include/helper.hpp
@@ -3,10 +3,11 @@
 
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <string>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void camera_callback(GLFWwindow *window, int key, int scancode, int action, int mods, glm::vec3 &camera_pos, glm::vec3 &camera_front, glm::vec3 &camera_up);
 void mouse_callback(GLFWwindow *window, double xpos, double ypos, float &yaw, float &pitch, glm::vec3 &camera_front, bool &reset_mouse_position);
-const char *load_shader(const char *shader_path);
+std::string load_shader(const char *shader_path);
 
 #endif
