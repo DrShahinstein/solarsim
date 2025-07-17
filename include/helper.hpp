b78af26bf65632e838d73b4eab2851456e816310
DrShahinstein <1taylanozturk@protonmail.com>
2025-07-17 22:55:25 +0300

    Create process_input() for keyboard actions

--- include/helper.hpp
+++ include/helper.hpp
@@ -4,10 +4,11 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <string>
+#include "camera.hpp"
+#include "mainloop.hpp"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void camera_callback(GLFWwindow *window, int key, int scancode, int action, int mods, glm::vec3 &camera_pos, glm::vec3 &camera_front, glm::vec3 &camera_up);
-void mouse_callback(GLFWwindow *window, double xpos, double ypos, float &yaw, float &pitch, glm::vec3 &camera_front, bool &reset_mouse_position);
 std::string load_shader(const char *shader_path);
+void process_input(GLFWwindow *window, Camera &camera, float delta_time, AppState *app);
 
 #endif
