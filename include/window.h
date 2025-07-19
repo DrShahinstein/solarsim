2d2f2bbfa7bb91f6a450099ee0b8302c3f5b4f82
DrShahinstein <1taylanozturk@protonmail.com>
2025-07-19 21:04:47 +0300

    Change initial window size in window.h

--- include/window.h
+++ include/window.h
@@ -1,8 +1,8 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
-#define SCREEN_WIDTH 1600
-#define SCREEN_HEIGHT 1200
+#define SCREEN_WIDTH  1200
+#define SCREEN_HEIGHT 800
 #define SCREEN_TITLE "Solarsim"
 
 #endif
