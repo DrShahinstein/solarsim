383f1e607227d1a6a10a9320cd73bc825760e347
DrShahinstein <1taylanozturk@protonmail.com>
2025-07-23 16:25:39 +0300

    Add new functions for enhanced gui control

--- include/gui.hpp
+++ include/gui.hpp
@@ -5,8 +5,16 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "simulation.hpp"
+#include "mainloop.hpp"
+
+#define UI_WIDTH         350.0f
+#define MIN_SLIDER_WIDTH 150.0f
 
 void initialize_imgui(GLFWwindow *window);
 void render_gui(AppState &app);
+void render_body_editor(CelestialBody &body, int index);
+void render_simulation_stats(AppState &app, double frame_time);
+void render_camera_info(Camera &camera);
 
 #endif
