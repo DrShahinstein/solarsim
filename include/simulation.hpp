d5942642eccd4f01a5116985c0f545fc8f132494
DrShahinstein <1taylanozturk@protonmail.com>
2025-07-23 16:26:43 +0300

    Update structure of class Simulation

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -4,6 +4,8 @@
 #include <glm/glm.hpp>
 #include <vector>
 
+#define DEFAULT_G 0.000295912208
+
 struct CelestialBody {
   glm::dvec3 position;
   glm::dvec3 velocity;
@@ -24,17 +26,19 @@ public:
   void add_body(const CelestialBody &body);
   void clear_bodies();
   void setG(double value);
+  double getG() const;
   std::vector<CelestialBody> &get_bodies();
   void update(double dt);
   void reset_to_solar_system();
+  void remove_marked_bodies();
   std::vector<CelestialBody> bodies;
-  double G;
 
 private:
   void compute_forces();
   void apply_post_newtonian_corrections();
   void integrate_velocity_verlet(double dt);
   Integrator current_integrator;
+  double G;
 };
 
 #endif
